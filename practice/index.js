0e61655ff929f35c6703501bb18fee4595d6555a
Manav Katrodiya <manavkatrodiya1010@gmail.com>
2026-02-11 23:34:20 +0530

    commit

---
+++ practice/index.js
@@ -0,0 +1,25 @@
+// require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/instagram", (req, res) => {
+  res.send("Hello manav_k_1010");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Please Login to continue</h1>");
+});
+
+app.get("/signup", (req, res) => {
+  res.send("<h1>If you dont have an account please Signup to continue</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
